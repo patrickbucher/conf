834bcd13113f701ab3f58a3252b6e49b889baab9
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-24 22:15:41 +0200

    extended gui setup script

--- setup-gui.sh
+++ setup-gui.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 sudo pacman -S xorg-server xf86-video-intel xorg-xinit xorg-xset xorg-xsetroot \
-	make gcc pkgconfig libxft libxinerama libxrandr
+	make gcc pkgconfig libxft libxinerama libxrandr \
+	xdg-user-dirs gtk-chtheme lxappearance gtk-engines
 
 mkdir -p ~/software
 cd ~/software
 rm -rf dwm dmenu st slock conf
 
-git clone https://suckless.org/dwm
-git clone https://suckless.org/dmenu
-git clone https://suckless.org/st
-git clone https://suckless.org/slock
-
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/slock
 git clone https://github.com/patrickbucher/conf
+
 cp ~/software/conf/dwm/config.h ~/software/dwm/
 cp ~/software/conf/st/config.h ~/software/st/
 cp ~/software/conf/slock/config.h ~/software/slock/
@@ -31,4 +32,10 @@ make && sudo make install
 
 cp ~/software/conf/.xinitrc ~/
 sudo cp ~/software/conf/scripts/switchkb /usr/bin/
-sudo ln -s /usr/bin/startx /usr/bin/x
+sudo ln -sf /usr/bin/startx /usr/bin/x
+
+cp ~/software/conf/user-dirs.dirs ~/.config/
+mkdir ~/downloads
+xdg-user-dirs-update
+
+rm -r ~/software/conf
