42c3697d1edbfce8fee7a0db60872ed3e7b4b591
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2020-03-02 19:35:37 +0100

    replaced st with qterminal (I like to have a scrollback buffer, after all)

--- dwm/config.h
+++ dwm/config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "qterminal", NULL };
 static const char *downvol[] = {"amixer", "set", "Master", "2-", "unmute", NULL};
 static const char *upvol[] = {"amixer", "set", "Master", "2+", "unmute", NULL};
 static const char *mute[] = {"amixer", "set", "Master", "mute", NULL};
