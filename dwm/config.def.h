df45f7bc7a501928f41f7ae4f44cd7fe64adbe08
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-15 19:53:50 +0200

    formatted code

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -59,10 +59,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *upvol[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "+1%", NULL};
-static const char *downvol[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "-1%", NULL};
-static const char *mute[] = {"pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
-static const char *switchkb[] = {"switchkb", NULL};
+static const char *upvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+1%", NULL };
+static const char *downvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-1%", NULL };
+static const char *mute[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
+static const char *switchkb[] = { "switchkb", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
